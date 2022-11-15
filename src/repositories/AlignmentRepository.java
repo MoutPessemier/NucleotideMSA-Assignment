fb8425a0189c0102eae676a84d0a888f9db2ec0d
Mout Pessemier <moutpessemier@hotmail.com>
2022-11-15 16:18:16 +0100

    Added docs

--- src/repositories/AlignmentRepository.java
+++ src/repositories/AlignmentRepository.java
@@ -5,7 +5,6 @@ import domain.alignment.StandardAlignment;
 
 // Keeps track of the optimal alignment in both notations
 public class AlignmentRepository {
-    // TODO: complete file
 
     private StandardAlignment optimalStandardAlignment;
     private SNPAlignment optimalSNPAlignment;
@@ -15,18 +14,38 @@ public class AlignmentRepository {
         setOptimalSNPAlignment(optimalSNPAlignment);
     }
 
+    /**
+     * Gets the standard format of the optimal alignment
+     *
+     * @return standard alignment of the optimal alignment
+     */
     public StandardAlignment getOptimalStandardAlignment() {
         return optimalStandardAlignment;
     }
 
+    /**
+     * Sets the standard representation of the optimal alignment
+     *
+     * @param optimalStandardAlignment standard representation of optimal alignment
+     */
     public void setOptimalStandardAlignment(StandardAlignment optimalStandardAlignment) {
         this.optimalStandardAlignment = optimalStandardAlignment;
     }
 
+    /**
+     * Gets the SNP format of the optimal alignment
+     *
+     * @return SNP alignment of the optimal alignment
+     */
     public SNPAlignment getOptimalSNPAlignment() {
         return optimalSNPAlignment;
     }
 
+    /**
+     * Sets the SNP representation of the optimal alignment
+     *
+     * @param optimalSNPAlignment SNP representation of optimal alignment
+     */
     public void setOptimalSNPAlignment(SNPAlignment optimalSNPAlignment) {
         this.optimalSNPAlignment = optimalSNPAlignment;
     }
