e712153ec7cc8e102f45b54486cf68bfc584df81
Mout Pessemier <moutpessemier@hotmail.com>
2022-12-06 12:47:13 +0100

    Forgot documentation

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -6,6 +6,12 @@ public class Utils {
     private static final String[] CHARACTERS = {"A", "C", "T", "G"};
     private static final Random RM = new Random();
 
+    /**
+     * Generates a random genome sequence with a ginven length
+     *
+     * @param length the length of the sequence
+     * @return a random genome sequecne
+     */
     public static String generateSequence(int length) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < length; i++) {
